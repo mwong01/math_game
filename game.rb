597318866911595082849abaf629ce06d58d5c80
mwong01 <mattw90@gmail.com>
2019-12-09 13:35:10 -0500

    first commit

---
+++ game.rb
@@ -0,0 +1,16 @@
+
+class game 
+  #  manage scores 
+end
+
+class turn 
+  #  renders after result of answered question 
+end
+
+class player
+  #  store p1 and p2 logic here 
+end
+
+class question
+  #  math question stored here 
+end
